d3f0ef104c302355e22ea56154fc5c2fdd03db75
Brandon Ramos <bramos5@huskers.unl.edu>
2021-03-29 18:59:37 -0500

    created tests for all methods

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace TravisCILab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_Valid_Ramos()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,54 +15,91 @@ namespace TravisCILab
         }
         
         [Test]
-        public void Subtract_valid()
+        public void Subtract_valid_Ramos()
         {
-            Assert.AreEqual(3, Program.Subtract("5", "2"));
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
         }
 
         [Test]
-        public void Multiply_valid()
+        public void Multiply_valid_Ramos()
         {
-            Assert.AreEqual(10, Program.Multiply("5", "2"));
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
         }
 
         [Test]
-        public void Divide_valid()
+        public void Divide_valid_Ramos()
         {
-            Assert.AreEqual(5, Program.Divide("10", "2"));
+            Assert.AreEqual(1, Program.Divide("1", "1"));
+            Assert.AreEqual(3, Program.Divide("6", "2"));
+            Assert.AreEqual(2, Program.Divide("10", "5"));
         }
 
         [Test]
-        public void Power_valid()
+        public void Power_valid_Ramos()
         {
-            Assert.AreEqual(16, Program.Power("2", "4"));
-
-            //make this test fail
-            Assert.AreEqual(13, Program.Power("2", "2"));
+            Assert.AreEqual(1, Program.Power("1", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(125, Program.Power("5", "3"));
         }
 
         [Test]
-        public void Add_Invalid()
-        {
+        public void Add_Invalid_Ramos()
+        {   //add
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
             Assert.Throws<FormatException>(() => Program.Add("a", "a"));
 
+            //subtract
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
 
+            //multiply
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
 
+            //divide
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+
+            //power
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
         }
 
         [Test]
-        public void Add_Null()
-        {
+        public void Add_Null_Ramos()
+        {   //add
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
-        }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+            //subtract
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+
+            //multiply
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+
+            //divide
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+
+            //power
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
